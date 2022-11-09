a60d6d113fd6e066e0950beb6151273db0212afb
Hridoy Banik <hridoy.banik71@gmail.com>
2022-11-09 15:36:00 +0600

    client side listing page added"

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper
+  } from '@mui/material'
+  import DeleteIcon from '@mui/icons-material/Delete';
+  import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 const Listing = () => {
+    const [person, setPerson] = useState([])
+
+    useEffect(()=> {
+        getCountries()
+    },[])
+    async function getCountries (){
+       const {data} =  await axios.get('http://localhost:8000/person')
+       setPerson(data)
+       
+    }
     return (
-        <div>
-            listing
-        </div>
+        <TableContainer sx={{ maxHeight: '300px' }} component={Paper}>
+      <Table stickyHeader aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Date of Birth</TableCell>
+            <TableCell>Country Name</TableCell>
+            <TableCell>PDF</TableCell>
+            <TableCell align='center'>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {person.map(row => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.dob}</TableCell>
+              <TableCell>{row.country}</TableCell>
+              <TableCell ><PictureAsPdfIcon/></TableCell>
+              <TableCell align='center'><DeleteIcon/></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     );
 };
 
